e2522fa2b170d16bff52213d2bb0803375e599c8
ucadena07 <ucadena07@gmail.com>
2022-10-08 11:15:25 -0500

    Exploring the authentication state provider

---
+++ Client/Auth/DummyAuthenticationStateProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorMovies.Client.Auth
+{
+    public class DummyAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(3000);
+            var anonymous = new ClaimsIdentity();
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymous)));
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,9 +1,11 @@
 using BlazorMovies.Client;
+using BlazorMovies.Client.Auth;
 using BlazorMovies.Client.Helpers;
 using BlazorMovies.Client.Repository;
 using BlazorMovies.Client.Repository.IRepository;
 using BlazorMovies.Client.Services;
 using BlazorMovies.Client.Services.IService;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,7 +13,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<SingletonService>();
 builder.Services.AddTransient<TransientService>();
 builder.Services.AddTransient<IRepository, Repository>();
@@ -19,5 +21,7 @@ builder.Services.AddScoped<IHttpService, HttpService>();
 builder.Services.AddScoped<IGenreRepository, GenreRepository>();
 builder.Services.AddScoped<IPersonRepository, PersonRepository>();
 builder.Services.AddScoped<IMovieRepository, MoviesRepository>();
+builder.Services.AddAuthorizationCore();
 
+builder.Services.AddScoped<AuthenticationStateProvider, DummyAuthenticationStateProvider>();
 await builder.Build().RunAsync();
