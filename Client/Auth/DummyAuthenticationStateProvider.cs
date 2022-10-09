1ff1d5c24412cdf23f8b795900d79f679ce6a38d
ucadena07 <ucadena07@gmail.com>
2022-10-09 08:06:19 -0500

    AuthorizeView - Hiding UI content for anonymous users

--- Client/Auth/DummyAuthenticationStateProvider.cs
+++ Client/Auth/DummyAuthenticationStateProvider.cs
@@ -8,7 +8,11 @@ namespace BlazorMovies.Client.Auth
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
             await Task.Delay(3000);
-            var anonymous = new ClaimsIdentity();
+            var anonymous = new ClaimsIdentity(new List<Claim>()
+            {
+                new Claim("key1","value1"),
+                new Claim(ClaimTypes.Name, "Ulises"),
+            });
             return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymous)));
         }
     }
